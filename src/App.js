822047049422cc880459639502c237bde75cf525
Cristian Jalba <cristian.jalba@everymatrix.com>
2020-11-02 21:24:25 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dragon is sleeping, SSSSSHHHH!!!
+          Doge is sleeping, SSSSSHHHHHHHH!!!
         </p>
         <a
           className="App-link"
